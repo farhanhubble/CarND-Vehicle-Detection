6f0bd24251dc5b17e0a487ea6a805a8b36b5a356
Farhan Ahmad <farhanhubble@gmail.com>
2017-06-21 05:45:19 +0530

    Add tests.

---
+++ test-detect.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from glob import glob
+from moviepy.editor import VideoFileClip
+
+import cv2
+import detect
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def extract_frames(filename):
+    os.makedirs('frames',exist_ok=True)
+    clip_in = VideoFileClip(filename,audio=False)
+    
+    nb_frames = int(clip_in.fps * clip_in.duration)
+    
+    for i in range(nb_frames):
+        time  = i/clip_in.fps
+        clip_in.save_frame('frames/frame{}.png'.format(i),time)
+        
+        
+if __name__ == '__main__':
+    extract_frames('test_video.mp4')
+    
+    filenames = glob('frames/*.png')
+    
+    model = detect.load_model('model.p')
+    scaler = detect.load_scaler('scaler.p')
+    
+    img_width = 1280
+    img_height = 720
+   
+    
+    os.makedirs('frames/boxes',exist_ok=True)
+    os.makedirs('frames/hmaps',exist_ok=True)
+    for filename in filenames:
+        
+        img = detect.imgread(filename)
+        bboxes = detect.search_vehicles(img,model,scaler)
+        drawn = detect.draw_bbox(img,bboxes)
+        path,name = os.path.split(filename)
+        cv2.imwrite(path+'/boxes/'+name,cv2.cvtColor(drawn,cv2.COLOR_RGB2BGR))
+        
+        heatmap = np.zeros([img_height,img_width])
+        detect.add_heat(heatmap,bboxes)
+        plt.imshow(heatmap,cmap='hot')
+        plt.savefig(path+'/hmaps/'+name)
+        
+        
+    
+
+
